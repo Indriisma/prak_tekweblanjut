2aa86f8b45abaf538f100af8c974a2f9fd424218
Indriisma <indrisakinahisma18@gmail.com>
2020-06-11 12:22:30 +0700

    prak_tekweblanjut

---
+++ tabel.php
@@ -0,0 +1,28 @@
+<html>
+<style>
+table, th, td {
+  border: 1px solid black;
+}
+</style>
+<body><table style="width:50%">
+
+  <tr>
+    <td>NIM</td>
+    <td><?php echo $_POST["NIM"]; ?></td> 
+     </tr>
+  <tr>
+    <td>Nama</td>
+    <td><?php echo $_POST["NAMA"]; ?></td>
+    </tr>
+  <tr>
+    <td>Jenis Kelamin</td>
+    <td><?php echo $_POST["KELAMIN"]; ?></td>
+    </tr>
+  <tr>
+    <td>Program Studi</td>
+    <td><?php echo $_POST["PRODI"]; ?></td>
+     </tr>
+     
+</table>
+</body>
+</html>
